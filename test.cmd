7fbd7ea1182629532492ffe6f41d3b1df6f65612
Simon Daniel <simon.daniel@fastmail.com>
2015-07-06 20:47:00 +1000

    Add some test commands, remove sudo

--- test.cmd
+++ test.cmd
@@ -1,4 +1,6 @@
 vagrant destroy -f
 vagrant box add --force boot2docker boot2docker.box
 vagrant up
-vagrant ssh -c "sudo docker run -it --rm hello-world"
+vagrant ssh -c "docker run -it hello-world"
+vagrant ssh -c "docker images"
+vagrant ssh -c "docker ps -a"
